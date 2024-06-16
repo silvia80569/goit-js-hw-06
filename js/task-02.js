136ca5c97bb5bfa6acb61b8d7d7afba1a9c5c99e
silvia80569 <157542289+silvia80569@users.noreply.github.com>
2024-06-16 19:52:40 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ulElement = document.querySelector("#ingredients");
+const fragment = document.createDocumentFragment();
+
+ingredients.forEach((ingredient, index) => {
+  const liElement = document.createElement("li");
+  liElement.textContent = `${-index -1} ${ingredient}`;
+  liElement.classList.add("item");
+  fragment.appendChild(liElement);
+});
+
+ulElement.appendChild(fragment);
